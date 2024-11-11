84d688410a2ebb5476e12494ae359e62fc22c946
Andrew Park <AndrewJHPark@protonmail.com>
2024-11-11 06:04:57 +0000

    user registration complete

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,25 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+//     As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. The body will contain a representation of a JSON Account, but will not contain an accountId.
+// - The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a JSON of the Account, including its accountId. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+// - If the registration is not successful due to a duplicate username, the response status should be 409. (Conflict)
+// - If the registration is not successful for some other reason, the response status should be 400. (Client error)
+    AccountService accountService;
+    public SocialMediaController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> registerAccount(@RequestBody Account newAccount) {
+    try {
+        Account registeredAccount = accountService.registerAccount(newAccount);
+        return ResponseEntity.status(HttpStatus.OK).body(registeredAccount);
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+    }
+}
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,34 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount(Account newAccount) {
+        if (newAccount.getUsername() == "") {
+            throw new IllegalArgumentException("Username cannot be blank.");
+        }
+
+        if (newAccount.getPassword().length() <= 4) {
+            throw new IllegalArgumentException("Password cannot be 4 or less in length.");
+        }
+
+        if (accountRepository.findByUsername(newAccount.getUsername()) == null) {
+            throw new IllegalArgumentException("User with that username already exists.");
+        }
+
+
+        return accountRepository.save(newAccount);
+    }
+
 }
